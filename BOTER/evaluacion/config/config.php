a69a8acbb7b0ec7bea6e50638328edcc5b40e54a
Edgar Cupul <a18016876@alumnos.uady.mx>
2023-01-24 07:26:09 -0600

    first commit

---
+++ BOTER/evaluacion/config/config.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_HOST", "bt7leuneoc4jmxliht6w-mysql.services.clever-cloud.com");
+define("DB_USER", "ulbyaom8rbdn95a0");
+define("DB_PASS", "jHqMmUkiLf73xtkkUPxK");
+define("DB_NAME", "bt7leuneoc4jmxliht6w");
+
