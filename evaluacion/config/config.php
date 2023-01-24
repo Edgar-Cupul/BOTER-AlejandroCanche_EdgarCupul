55068bae4e2e955e596b24e656d77537a34cd592
Edgar Cupul <a18016876@alumnos.uady.mx>
2023-01-24 08:22:03 -0600

    Cambio en la base de datos

--- evaluacion/config/config.php
+++ evaluacion/config/config.php
@@ -1,6 +1,12 @@
 <?php
-define("DB_HOST", "bt7leuneoc4jmxliht6w-mysql.services.clever-cloud.com");
-define("DB_USER", "ulbyaom8rbdn95a0");
-define("DB_PASS", "jHqMmUkiLf73xtkkUPxK");
-define("DB_NAME", "bt7leuneoc4jmxliht6w");
+//define("DB_HOST", "bt7leuneoc4jmxliht6w-mysql.services.clever-cloud.com");
+//define("DB_USER", "ulbyaom8rbdn95a0");
+//define("DB_PASS", "jHqMmUkiLf73xtkkUPxK");
+//define("DB_NAME", "bt7leuneoc4jmxliht6w");
+define("MYSQL_ADDON_HOST", "bt7leuneoc4jmxliht6w-mysql.services.clever-cloud.com");
+define("MYSQL_ADDON_DB", "bt7leuneoc4jmxliht6w");
+define("MYSQL_ADDON_USER", "ulbyaom8rbdn95a0");
+define("MYSQL_ADDON_PORT", "3306");
+define("MYSQL_ADDON_PASSWORD", "jHqMmUkiLf73xtkkUPxK");
+define("MYSQL_ADDON_URI", "mysql://ulbyaom8rbdn95a0:jHqMmUkiLf73xtkkUPxK@bt7leuneoc4jmxliht6w-mysql.services.clever-cloud.com:3306/bt7leuneoc4jmxliht6w");
 
